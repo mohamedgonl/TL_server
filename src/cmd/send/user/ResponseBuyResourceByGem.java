8f9968b82adb02bfabcaec4212e8a89153b931e6
QuyetND2 <quyetnd2@vng.com.vn>
2023-10-13 17:57:42 +0700

    add gold, elixir in buy res by gem

--- src/cmd/send/user/ResponseBuyResourceByGem.java
+++ src/cmd/send/user/ResponseBuyResourceByGem.java
@@ -8,17 +8,25 @@ import java.nio.ByteBuffer;
 public class ResponseBuyResourceByGem extends BaseMsg {
     private int gem;
 
+    private int gold;
+
+    private int elixir;
+
     public ResponseBuyResourceByGem(short error) {super(CmdDefine.BUY_RESOURCE_BY_GEM, error);}
 
-    public ResponseBuyResourceByGem(short error, int gem) {
+    public ResponseBuyResourceByGem(short error, int gem, int gold, int elixir) {
         super(CmdDefine.BUY_RESOURCE_BY_GEM, error);
         this.gem = gem;
+        this.gold = gold;
+        this.elixir = elixir;
     }
 
     @Override
     public byte[] createData() {
         ByteBuffer bf = makeBuffer();
         bf.putInt(this.gem);
+        bf.putInt(this.gold);
+        bf.putInt(this.elixir);
         return packBuffer(bf);
     }
 }
