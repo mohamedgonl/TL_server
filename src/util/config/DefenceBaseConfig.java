a9a89528dce75f07d5665d07526883c3d2d4343a
CPU60369\CPU60369-Local <tuanlv2@vng.com.vn>
2023-11-17 16:36:00 +0700

    update: log

--- src/util/config/DefenceBaseConfig.java
+++ src/util/config/DefenceBaseConfig.java
@@ -1,11 +1,11 @@
 package util.config;
 
 public class DefenceBaseConfig {
-    public float minRange;
-    public float maxRange;
-    public float attackSpeed;
-    public float attackRadius;
-    public float attackArea;
-    public float attackType;
+    public double minRange;
+    public double maxRange;
+    public double attackSpeed;
+    public double attackRadius;
+    public int attackArea;
+    public int attackType;
 
 }
